71051edc2cee6c45280d1f9a5f8b82e615bf5a8e
Refael Ackermann <refack@gmail.com>
2017-06-29 07:28:12 -0400

    fix path discovery

--- vswhere_usability_wrapper.cmd
+++ vswhere_usability_wrapper.cmd
@@ -1,12 +1,12 @@
 :: Copyright 2017 - Refael Ackermann
 :: Distributed under MIT style license
 :: See accompanying file LICENSE at https://github.com/node4good/windows-autoconf
-:: version: 1.15.2
+:: version: 1.15.3
 
 @if not defined DEBUG_HELPER @ECHO OFF
 setlocal
 set "InstallerPath=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer"
-if not exist "%InstallerPath%" set "VSWHERE=%ProgramFiles%\Microsoft Visual Studio\Installer"
+if not exist "%InstallerPath%" set "InstallerPath=%ProgramFiles%\Microsoft Visual Studio\Installer"
 if not exist "%InstallerPath%" exit goto :no-vswhere
 :: Manipulate %Path% for easier " handeling
 set Path=%Path%;%InstallerPath%
